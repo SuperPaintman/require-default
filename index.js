3942268fe95843ffae32ad5f97bb8f286d2c1e62
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-04-28 07:39:42 +0500

    Added jsdoc

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/**
+ * Require ES2015 default module
+ * 
+ * @param  {String} p - path to module.
+ * @return {Any}        module
+ *
+ * @example
+ * const m = requireDefault('lib/index.js');
+ */
 function requireDefault(p) {
   const m = require(p);
 
