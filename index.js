1117bca5f33107cef3b8aa6f513b195ad0a0f7ca
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2016-04-28 08:42:20 +0500

    Fixed invalid error ignoring

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
-
+/** Requires */
 var path = require('path');
+
+/** Constants */
+var ERROR_CODE_MODULE_NOT_FOUND = 'MODULE_NOT_FOUND';
 
 /**
  * Require ES2015 default module
@@ -17,7 +20,12 @@ function requireDefault(p) {
   try {
     m = require(path.join(path.dirname(module.parent.filename), p));
   } catch (err) {
-    m = require(p);
+    /** Validate error */
+    if (err.code === ERROR_CODE_MODULE_NOT_FOUND) {
+      m = require(p);
+    } else {
+      throw err;
+    }
   }
 
   return m && m.default ? m.default : m;
